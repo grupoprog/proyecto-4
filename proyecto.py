fbb75b185826ae5b23fbb192712b788c7ca26cda
tomas-estibiarria <estibiarriatomas@gmail.com>
2026-06-10 21:49:15 +0000

    Modificación del diseño de datos.

--- proyecto.py
+++ proyecto.py
@@ -1,17 +1,33 @@
+# -------------------------------------------------------------------------------------------------------------------
+# Diseño de datos:
+
+# Representamos una "tabla" (dataset) como una lista de diccionarios, donde cada
+# diccionario es una "fila" de la tabla, siendo de la forma: [{atributo: valor}]
+
+# En este caso, los "atributos" son los identificadores de cada columna, por lo que serán
+# representados por Strings (ej: "Timestamp", "City", "European_AQI", etc.)
+
+# los "valores" dependeran del atributo al que correspondan, siendo entonces:
+
+# Fechas (Timestamp): Tuplas de la forma (Año,Mes,Dia,Hora), donde cada una 
+# es un entero
+
+# Ciudades (City): Strings
+
+# Valores correspondientes a Latitude, Longitude, PM10_ug_m3, PM2_5_ug_m3, Nitrogen_Dioxide_ug_m3, UV_Index: Float
+
+# valores correspondientes a Carbon_Monoxide_ug_m3, Ozone_ug_m3, Dust_ug_m3, European_AQI, Hazardous_Event: Int 
+# -------------------------------------------------------------------------------------------------------------------
 import streamlit as st
-'''
-Representamos la tabla como una lista y cada fila de la tabla como {identificador: dato}
-donde,
-    identificador: (string) es el nombre de cada atributo de la tabla
-    dato: (string) son los valores de las celdas de cada atributo correspondiente
-'''
+
+
 def procesar_archivo(nombre: str) -> list[dict]:
     '''
     archivo: string
 
     archivo -> tabla
 
-    Recibe el nombre del archivo y produce una tabla con las entradas
+    Recibe el nombre del archivo y produce una tabla con las entradas.
     '''
     archivo = open(nombre)
     identificadores = archivo.readline().rstrip("\n").split(",")     # obtengo los identificadores
@@ -28,19 +44,20 @@ def procesar_archivo(nombre: str) -> list[dict]:
     archivo.close()
     return datos
 
+
 def filtrar_por_año(tabla: list[dict],año: int) -> list[dict]:
     """
     tabla entero -> tabla
+    
     Recibe una tabla y un año, y produce una tabla con todas las entradas coincidentes a ese año
     """
-    lista_nueva=[]
+    lista_nueva = []
     for e in tabla:
         if  e["Timestamp"][:4] == año:
             lista_nueva.append(e)
     return lista_nueva
 
 
-    
 def main():
     #python -m streamlit run app.py para ejecutar la aplicación
     #tabla = procesar_archivo("global_urban_smog_pm25_hourly_12k.csv")
@@ -52,6 +69,7 @@ def main():
     #{"Nombre": "Oscar", "Edad":56, "Le gusta jugar?":"no"}]
     #con la siguiente funcion ya se ve una tabla en la pagina
     #st.table(ejemplo)
+
 
 if __name__ == "__main__":
     main()
