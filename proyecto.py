ec82933f31a6bd11807051251b6d556c20edbac7
Jazmin-Ruelli-Cubertie <jmcubertie@gmail.com>
2026-07-07 22:07:10 +0000

    funcion aux

--- proyecto.py
+++ proyecto.py
@@ -489,7 +489,7 @@ def listar_por_atributo(tabla: list[dict], atributo: str) -> list:
     return lista
 
 
-def filtar_promedio_ciudades_por_fecha(tabla: list[dict], atributo: str, fecha: str) -> dict[str: float]:
+def filtrar_promedio_ciudades_por_fecha(tabla: list[dict], atributo: str, fecha: str) -> dict[str: float]:
     '''
     tabla atributo fecha("mes año") -> {ciudad: promedio}
 
@@ -514,8 +514,23 @@ def filtar_promedio_ciudades_por_fecha(tabla: list[dict], atributo: str, fecha: 
 
     return promedio_ciudades
 
+def aux(dic_ciudad_prom: dict, componente:str,dic: dict)-> dict[str:list[float]]:
+    '''toma un diccionario de la forma {'ciudad':promedio de atributo dado} y componente
+    y para cada ciudad en el diccionario recibido como argumento, va agregando un elemento a la lista (el promedio del componente dado)
+    por ejemplo, al filtrar el promedio de "PM10_ug_m3" en  "Diciembre 2025" de una tabla dada, tenemos que 
+    dic_ciudad_prom={"Delhi": 129.5, "Beijing": 164.4}
+    aux({"Delhi": 129.5, "Beijing": 164.4},"PM10_ug_m3")=={"Delhi":[129.5], "Beijing":[164.4]} '''
+    for ciudad in dic_ciudad_prom:
+        promedio = dic_ciudad_prom[ciudad]
 
-def promedios_componentes_de_ciudades(tabla: list[dict], fecha: str) -> dict[list[float]]:
+        if ciudad in dic:
+            dic[ciudad].append(promedio)
+        else:
+            dic[ciudad] = [promedio]
+
+    return dic
+
+def promedios_componentes_de_ciudades(tabla: list[dict], fecha: str) -> dict[str:list[float]]:
     '''
     tabla fecha -> {ciudad: [promedios]}
 
@@ -524,20 +539,13 @@ def promedios_componentes_de_ciudades(tabla: list[dict], fecha: str) -> dict[lis
     las componentes del aire correspondientes a la misma en la fecha dada.
     '''
     dic = {}
-    componentes = ["PM10_ug_m3", "PM2_5_ug_m3", "Carbon_Monoxide_ug_m3", "Nitrogen_Dioxide_ug_m3",
-                   "Ozone_ug_m3", "Dust_ug_m3"]
+    
+    componentes = ["PM10_ug_m3", "PM2_5_ug_m3", "Carbon_Monoxide_ug_m3", "Nitrogen_Dioxide_ug_m3","Ozone_ug_m3", "Dust_ug_m3"]
     
     for componente in componentes:
-        promedios_componente = filtar_promedio_ciudades_por_fecha(tabla,componente,fecha)
-
-        for ciudad in promedios_componente:
-            promedio = promedios_componente[ciudad]
-
-            if ciudad in dic:
-                dic[ciudad].append(promedio)
-            else:
-                dic[ciudad] = [promedio]
-    
+        promedios_componente = filtrar_promedio_ciudades_por_fecha(tabla,componente,fecha)
+        dic=aux(promedios_componente,componente,dic)
+        
     return dic
 
 
