5af24d4b9ccb454b49a5434852a0527728d11fea
tomas-estibiarria <estibiarriatomas@gmail.com>
2026-07-08 01:08:57 +0000

    Implementación de descripción de colores de indices UV

--- proyecto.py
+++ proyecto.py
@@ -29,6 +29,7 @@
 # -------------------------------------------------------------------------------------------------------------------
 import streamlit as st
 import matplotlib.pyplot as plt
+import matplotlib.patches as pat
 
 def fechaAtupla(fecha: str) -> tuple[int,int,int,int]:
     '''
@@ -446,6 +447,51 @@ def ejecutar_pregunta1(tabla: list[dict]):
     st.table(pregunta_1(tabla, 2025))
 
 
+def colores_ind_uv(list_colores: list, etiquetas: list, ncols: int = 5):
+    '''
+    Realiza una descripcion de los significados de cada color de los indices UV
+    '''
+    cell_width = 180
+    cell_height = 22
+    swatch_width = 30
+    margin = 5
+
+    n = len(etiquetas)
+    nrows = pat.math.ceil(n / ncols)
+
+    width = cell_width * ncols + 2 * margin
+    height = cell_height * nrows + 2 * margin
+    dpi = 80
+
+    fig, ax = plt.subplots(figsize=(width / dpi, height / dpi), dpi=dpi)
+    fig.subplots_adjust(margin/width, margin/height,
+                        (width-margin)/width, (height-margin)/height)
+    ax.set_xlim(0, cell_width * ncols)
+    ax.set_ylim(cell_height * (nrows-0.5), -cell_height/2.)
+    ax.yaxis.set_visible(False)
+    ax.xaxis.set_visible(False)
+    ax.set_axis_off()
+
+    for i, name in enumerate(etiquetas):
+        row = i % nrows
+        col = i // nrows
+        y = row * cell_height
+
+        swatch_start_x = cell_width * col
+        text_pos_x = cell_width * col + swatch_width + 7
+
+        ax.text(text_pos_x, y, name, fontsize=14,
+                horizontalalignment='left',
+                verticalalignment='center')
+
+        ax.add_patch(
+            pat.Rectangle(xy=(swatch_start_x, y-9), width=swatch_width,
+                      height=18, facecolor=list_colores[i], edgecolor='0.7')
+        )
+
+    return fig
+
+
 def ejecutar_pregunta2(tabla: list[dict]):
     '''
     Permite la entrada de la fecha y produce los componentes de la pregunta 2 en la pagina
@@ -458,6 +504,13 @@ def ejecutar_pregunta2(tabla: list[dict]):
     
     opcion = st.selectbox("Elija un mes:", meses)
     st.write("Mapa del promedio de UV en el mes de", opcion)
+
+    colores = ["#0DFF00C8","#FFFF00C8","#FF9900C7","#FF0000C7","#C300FFC6"]
+    etiquetas = ["Bajo", "Moderado", "Alto", "Muy alto", "Extremo"]
+
+    fig = colores_ind_uv(colores, etiquetas)
+
+    st.pyplot(fig)
         
     ubicaciones_promedios = filtrar_ubicacionesXmes(tabla, opcion)
         
--- prueba.py
+++ prueba.py
@@ -57,6 +57,52 @@ def plot_colortable(colors, *, ncols=4, sort_colors=True):
 
     return fig
 
+colores = ["#0DFF00C8","#FFFF00C8","#FF9900C7","#FF0000C7","#C300FFC6"]
+etiquetas = ["Bajo", "Moderado", "Alto", "Muy alto", "Extremo"]
 
+def colores_ind_uv(list_colores: list, etiquetas: list, ncols: int = 5):
+    cell_width = 212
+    cell_height = 22
+    swatch_width = 30
+    margin = 5
 
+    n = len(etiquetas)
+    nrows = math.ceil(n / ncols)
 
+    width = cell_width * ncols + 2 * margin
+    height = cell_height * nrows + 2 * margin
+    dpi = 80
+
+    fig, ax = plt.subplots(figsize=(width / dpi, height / dpi), dpi=dpi)
+    fig.subplots_adjust(margin/width, margin/height,
+                        (width-margin)/width, (height-margin)/height)
+    ax.set_xlim(0, cell_width * ncols)
+    ax.set_ylim(cell_height * (nrows-0.5), -cell_height/2.)
+    ax.yaxis.set_visible(False)
+    ax.xaxis.set_visible(False)
+    ax.set_axis_off()
+
+    for i, name in enumerate(etiquetas):
+        row = i % nrows
+        col = i // nrows
+        y = row * cell_height
+
+        swatch_start_x = cell_width * col
+        text_pos_x = cell_width * col + swatch_width + 7
+
+        ax.text(text_pos_x, y, name, fontsize=14,
+                horizontalalignment='left',
+                verticalalignment='center')
+
+        ax.add_patch(
+            Rectangle(xy=(swatch_start_x, y-9), width=swatch_width,
+                      height=18, facecolor=list_colores[i], edgecolor='0.7')
+        )
+
+    return fig
+
+fig_2 = colores_ind_uv(colores,etiquetas)
+fig_2.savefig("UV.png")
+
+#fig = plot_colortable(mcolors.CSS4_COLORS)
+#fig.savefig("Colores.png")
